81d50c06b9cd579915dd1d0534961c6e97bffbd3
Lajos Santa <santa.lajos@coldline.hu>
2015-04-05 09:54:16 +0200

    Add author to package

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name = "Sqlchemyforms",
-    description = "Generate form objects for create HTML forms based on SQLAlchhemy models",
     version = '1.0',
+    description = "Generate form objects for create HTML forms based on SQLAlchhemy models",
+    author = 'Lajos Santa',
+    author_email = 'santa.lajos@coldline.hu',
+    url = 'https://github.com/voidpp/sqlchemyforms',
+    license = 'MIT',
     install_requires = [
         "SQLAlchemy >= 0.9.8",
     ],
-    packages = find_packages()
+    packages = find_packages(),
 )
 
